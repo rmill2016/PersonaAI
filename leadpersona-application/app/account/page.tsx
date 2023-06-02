93c18b3b968ea44c285e1ee1d41e3e0536e5ca38
Ryan Mill <rmill2016@gmail.com>
2023-06-02 10:08:35 -0700

    start leadpersona web application

---
+++ leadpersona-application/app/account/page.tsx
@@ -0,0 +1,185 @@
+import ManageSubscriptionButton from './ManageSubscriptionButton';
+import {
+  getSession,
+  getUserDetails,
+  getSubscription
+} from '@/app/supabase-server';
+import Button from '@/components/ui/Button';
+import { Database } from '@/types_db';
+import { createServerActionClient } from '@supabase/auth-helpers-nextjs';
+import { revalidatePath } from 'next/cache';
+import { cookies } from 'next/headers';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import { ReactNode } from 'react';
+
+export default async function Account() {
+  const [session, userDetails, subscription] = await Promise.all([
+    getSession(),
+    getUserDetails(),
+    getSubscription()
+  ]);
+
+  const user = session?.user;
+
+  if (!session) {
+    return redirect('/signin');
+  }
+
+  const subscriptionPrice =
+    subscription &&
+    new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: subscription?.prices?.currency!,
+      minimumFractionDigits: 0
+    }).format((subscription?.prices?.unit_amount || 0) / 100);
+
+  const updateName = async (formData: FormData) => {
+    'use server';
+
+    const newName = formData.get('name') as string;
+    const supabase = createServerActionClient<Database>({ cookies });
+    const session = await getSession();
+    const user = session?.user;
+    const { error } = await supabase
+      .from('users')
+      .update({ full_name: newName })
+      .eq('id', user?.id);
+    if (error) {
+      console.log(error);
+    }
+    revalidatePath('/account');
+  };
+
+  const updateEmail = async (formData: FormData) => {
+    'use server';
+
+    const newEmail = formData.get('email') as string;
+    const supabase = createServerActionClient<Database>({ cookies });
+    const { error } = await supabase.auth.updateUser({ email: newEmail });
+    if (error) {
+      console.log(error);
+    }
+    revalidatePath('/account');
+  };
+
+  return (
+    <section className="mb-32 bg-black">
+      <div className="sm:px-6 sm:pt-24 lg:px-8 max-w-6xl px-4 py-8 mx-auto">
+        <div className="sm:align-center sm:flex sm:flex-col">
+          <h1 className="sm:text-center sm:text-6xl text-4xl font-extrabold text-white">
+            Account
+          </h1>
+          <p className="text-zinc-200 sm:text-center sm:text-2xl max-w-2xl m-auto mt-5 text-xl">
+            We partnered with Stripe for a simplified billing.
+          </p>
+        </div>
+      </div>
+      <div className="p-4">
+        <Card
+          title="Your Plan"
+          description={
+            subscription
+              ? `You are currently on the ${subscription?.prices?.products?.name} plan.`
+              : 'You are not currently subscribed to any plan.'
+          }
+          footer={<ManageSubscriptionButton session={session} />}
+        >
+          <div className="mt-8 mb-4 text-xl font-semibold">
+            {subscription ? (
+              `${subscriptionPrice}/${subscription?.prices?.interval}`
+            ) : (
+              <Link href="/">Choose your plan</Link>
+            )}
+          </div>
+        </Card>
+        <Card
+          title="Your Name"
+          description="Please enter your full name, or a display name you are comfortable with."
+          footer={
+            <div className="sm:flex-row sm:items-center flex flex-col items-start justify-between">
+              <p className="sm:pb-0 pb-4">64 characters maximum</p>
+              <Button
+                variant="slim"
+                type="submit"
+                form="nameForm"
+
+              >
+
+                Update Name
+              </Button>
+            </div>
+          }
+        >
+          <div className="mt-8 mb-4 text-xl font-semibold">
+            <form id="nameForm" action={updateName}>
+              <input
+                type="text"
+                name="name"
+                className="bg-zinc-800 w-1/2 p-3 rounded-md"
+                defaultValue={userDetails?.full_name ?? ''}
+                placeholder="Your name"
+                maxLength={64}
+              />
+            </form>
+          </div>
+        </Card>
+        <Card
+          title="Your Email"
+          description="Please enter the email address you want to use to login."
+          footer={
+            <div className="sm:flex-row sm:items-center flex flex-col items-start justify-between">
+              <p className="sm:pb-0 pb-4">
+                We will email you to verify the change.
+              </p>
+              <Button
+                variant="slim"
+                type="submit"
+                form="emailForm"
+
+              >
+
+                Update Email
+              </Button>
+            </div>
+          }
+        >
+          <div className="mt-8 mb-4 text-xl font-semibold">
+            <form id="emailForm" action={updateEmail}>
+              <input
+                type="text"
+                name="email"
+                className="bg-zinc-800 w-1/2 p-3 rounded-md"
+                defaultValue={user ? user.email : ''}
+                placeholder="Your email"
+                maxLength={64}
+              />
+            </form>
+          </div>
+        </Card>
+      </div>
+    </section>
+  );
+}
+
+interface Props {
+  title: string;
+  description?: string;
+  footer?: ReactNode;
+  children: ReactNode;
+}
+
+function Card({ title, description, footer, children }: Props) {
+  return (
+    <div className="p border-zinc-700 w-full max-w-3xl m-auto my-8 border rounded-md">
+      <div className="px-5 py-4">
+        <h3 className="mb-1 text-2xl font-medium">{title}</h3>
+        <p className="text-zinc-300">{description}</p>
+        {children}
+      </div>
+      <div className="rounded-b-md border-zinc-700 bg-zinc-900 text-zinc-500 p-4 border-t">
+        {footer}
+      </div>
+    </div>
+  );
+}
